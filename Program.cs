4595aaae38faf59f3204702a2e83c4b6f61bd3c9
Maha <91832344+Maha1431@users.noreply.github.com>
2021-12-04 17:12:34 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddressbookModel model = new AddressbookModel();
+            EmployeeRepo repo = new EmployeeRepo();
+            repo.Add(model);
+            repo.DeletePerson(model);
+            repo.UpdateData(model);
+            repo.RetrivePersonCityOrState(model);
+        }
+    }
+}
